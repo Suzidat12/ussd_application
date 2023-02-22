3d3de9574d70da1a3f36e2dfd7247a1dcc770722
olasunkanmi <olasunkanmizik@gmail.com>
2023-02-22 11:52:13 +0100

    Added ussd application project

---
+++ ussd_application/src/test/java/com/zik/ussd_application/UssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zik.ussd_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
