ed1fb0cc57fe996192bb4a56722e550cafaf8301
olasunkanmi <olasunkanmizik@gmail.com>
2023-02-22 13:23:55 +0100

    Added create account method and update create account method to the account service

--- ussd_application/src/main/java/com/zik/ussd_application/service/AccountService.java
+++ ussd_application/src/main/java/com/zik/ussd_application/service/AccountService.java
@@ -1,4 +1,9 @@
 package com.zik.ussd_application.service;
 
+import com.zik.ussd_application.dto.AccountDto;
+import org.springframework.http.ResponseEntity;
+
 public interface AccountService {
+    public ResponseEntity createAccount(AccountDto load);
+    public ResponseEntity updateCreatedAccount(AccountDto load, Long id);
 }
--- ussd_application/src/main/java/com/zik/ussd_application/serviceImpl/AccountServiceImpl.java
+++ ussd_application/src/main/java/com/zik/ussd_application/serviceImpl/AccountServiceImpl.java
@@ -1,8 +1,19 @@
 package com.zik.ussd_application.serviceImpl;
 
+import com.zik.ussd_application.dto.AccountDto;
 import com.zik.ussd_application.service.AccountService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+    @Override
+    public ResponseEntity createAccount(AccountDto load) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity updateCreatedAccount(AccountDto load, Long id) {
+        return null;
+    }
 }
