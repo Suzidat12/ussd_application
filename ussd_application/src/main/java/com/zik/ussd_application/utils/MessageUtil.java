f3368f847d9d92769d06f71f2821e2108951fc54
olasunkanmi <olasunkanmizik@gmail.com>
2023-02-23 21:47:19 +0100

    Make changes to message util

--- ussd_application/src/main/java/com/zik/ussd_application/utils/MessageUtil.java
+++ ussd_application/src/main/java/com/zik/ussd_application/utils/MessageUtil.java
@@ -4,7 +4,7 @@ public class MessageUtil {
 
     public final static String DUPLICATE_ACCOUNT= "Phone number already attached to an account number";
     public final static String ACCOUNT_CREATED= "Account has been successfully created";
-    public final static String ACCOUNT_NOT_FOUND= "Account not exist for deposit";
+    public final static String ACCOUNT_NOT_FOUND= "Account not exist";
     public final static String ACCOUNT_UPDATED= "Account updated successfully";
     public final static String ACCOUNT_DEPOSIT= "Deposit of  ₦";
     public final static String ACCOUNT_WITHDRAW= "Withdrawal of  ₦";
